349f5b55465abf53cb7c88b3fa8adbe357085805
Maria <112690332+mashakoloss@users.noreply.github.com>
2022-12-01 22:55:41 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+using System;
+
+namespace HelloWorldApp
+{
+
+    class Geeks
+    {
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
+        }
+    }
+}
