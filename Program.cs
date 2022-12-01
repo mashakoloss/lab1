04f271dde33f8d02b22c7dc1d43b7233d426e127
Maria <112690332+mashakoloss@users.noreply.github.com>
2022-12-01 22:56:41 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,3 @@
-﻿namespace ConsoleApp1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
 using System;
 
 namespace HelloWorldApp
